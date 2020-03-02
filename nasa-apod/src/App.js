578fbb8ce2e25aade4a3ef9dde47ce3c37125587
0pippi0 <j.romany91@gmail.com>
2020-03-03 02:20:30 +0900

    <ViewerTemplate 컴포넌트 완성하기> space-navigator-wrapper 를 viewer-wrapper 내부에 넣어준 이유는, 추후 SpaceNavigator 컴포넌트에서 위치선정을 하게 될 때 viewer-wrapper 의 크기에 기반하여 설정 할 것이기 때문

--- nasa-apod/src/App.js
+++ nasa-apod/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
+import ViewerTemplate from './components/ViewerTemplate';
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <ViewerTemplate />
     );
   }
 }
