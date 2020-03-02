4a5a323efa725aa741b8d3a2e36b3de69c0df337
0pippi0 <j.romany91@gmail.com>
2020-03-03 01:56:56 +0900

    <컴포넌트 틀 준비하기> Viewer 이미지 혹은 동영상을 보여주는 컴포넌트입니다. 데이터의 형식은 mediaType 에 “video” 혹은 “image” 라는 값으로 전달이 될 것이고, 이에 따라 url 을 사용하여 동영상이나 이미지를 보여주게 됩니다. 추가적으로, loading 값은 데이터를 불러올 때 로딩표시를 하기 위하여 사용되는 props

---
+++ nasa-apod/src/components/Viewer/Viewer.js
@@ -0,0 +1,17 @@
+import React, { createContext } from 'react';
+import styles from './Viewer.scss';
+import classNames from 'classnames/bind';
+
+
+const cx = classNames.bind(styles);
+
+const Viewer = ({ mediaType, url, loading }) => {
+    return (
+        <div className={cx('viewer')}>
+
+        </div>
+    );
+};
+
+
+export default Viewer;
