712528c3ebdf409be33ab7a5b89b4b5ff9d4bcf1
0pippi0 <j.romany91@gmail.com>
2020-03-03 02:47:59 +0900

    <SpaceNavigator 완성하기> 좌측과 우측에 버튼을 만들어 줄 것인데요, 이 과정에서 우리는 react-icons 에서 받은 아이콘을 사용

--- nasa-apod/src/components/SpaceNavigator/SpaceNavigator.js
+++ nasa-apod/src/components/SpaceNavigator/SpaceNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './SpaceNavigator.scss';
 import classNames from 'classnames/bind';
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 
 const cx = classNames.bind(styles);
@@ -8,7 +9,16 @@ const cx = classNames.bind(styles);
 const SpaceNavigator = ({ onPrev, onNext }) => {
     return(
         <div className={cx('space-navigator')}>
-
+            <div className={cx('left', 'end')}>
+                <div className={cx('circle')} onClick={onPrev}>
+                    <FiChevronLeft />
+                </div>
+            </div>
+            <div className={cx('right', 'end')}>
+                <div className={cx('circle')} onClick={onNext}>
+                    <FiChevronRight />
+                </div>
+            </div>
         </div>
     );
 };
