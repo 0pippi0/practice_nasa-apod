96501ce0a19bedace2d830a44ea82ec50b4aba3d
0pippi0 <j.romany91@gmail.com>
2020-03-03 01:49:21 +0900

    <컴포넌트 틀 준비하기> ViewerTemplate 템플릿 컴포넌트로서 JSX 형태의 props 인 viewer, spaceNavigator 를 받아와서 적당한 위치에 렌더링

---
+++ nasa-apod/src/components/ViewerTemplate/ViewerTemplate.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './ViewerTemplate.scss';
+import classNames from 'classnames/bind';
+
+
+const cx = classNames.bind(styles);
+
+const ViewerTemplate = ({ viewer, spaceNavigator }) => {
+    return (
+        <div className={cx('viewer-template')}>
+
+        </div>
+    );
+};
+
+
+export default ViewerTemplate;
