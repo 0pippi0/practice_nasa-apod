ba10bdb59d634bf314d60ee69070ca7447213a36
0pippi0 <j.romany91@gmail.com>
2020-03-03 01:31:26 +0900

    @import 'utils'; 형태로 불러 올 수 있게 해주는 설정
    <프로젝트 초기화>
    불필요한 파일들을 제거
    App 컴포넌트
    index.css 를 불러오는 코드를 지워
    
    <스타일 유틸과 base 스타일 설정>
    페이지의 기반 스타일링을 할 base 파일과, 추후 유틸로 불러와서 사용할 utils 파일을 생성
    경로에 ~ 이 들어간다는 것은, node_modules 내부의 디렉토리를 사용한다는 것
    여기서 사용한 $oc-gray-8 이 open-color 에 등록된 색상 변수입니다.
    open-color 에 등록된 변수를 사용하려면, 가이드를 참고하여 거기서 나타나는 색상값과 명암값을 다음과 같은 형식으로 변수명으로 작성하면 됩니다: $oc-[색상]-[명암]

--- nasa-apod/config/webpack.config.js
+++ nasa-apod/config/webpack.config.js
@@ -455,18 +455,24 @@ module.exports = function(webpackEnv) {
             {
               test: sassRegex,
               exclude: sassModuleRegex,
-              use: getStyleLoaders(
-                {
-                  importLoaders: 3,
+              use: getStyleLoaders({
+                importLoaders: 2,
+                sourceMap: isEnvProduction && shouldUseSourceMap
+              }).concat({
+                loader: require.resolve('sass-loader'),
+                options: {
+                  prependData: `@import 'utils';`,
+                  sassOptions:{
+                  includePaths: [paths.appSrc + '/styles'],
                   sourceMap: isEnvProduction && shouldUseSourceMap,
-                },
-                'sass-loader'
-              ),
+                  }
+                }
+              }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
               // Remove this when webpack adds a warning or an error for this.
               // See https://github.com/webpack/webpack/issues/6571
-              sideEffects: true,
+              sideEffects: true
             },
             // Adds support for CSS Modules, but using SASS
             // using the extension .module.scss or .module.sass
