96f592e4a1a99edb0a82b5856a20b44bddf7abdc
0pippi0 <j.romany91@gmail.com>
2020-03-04 04:49:02 +0900

    Updates

---
+++ precache-manifest.443e4f9811e5f52c2f562d1206e3034b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82ecfa075b8e6d3b15131b3d68747e2",
+    "url": "/practice_nasa-apod/index.html"
+  },
+  {
+    "revision": "4a380731d42d9aba5374",
+    "url": "/practice_nasa-apod/static/css/main.14100fa5.chunk.css"
+  },
+  {
+    "revision": "375d9180d9badaa7e9df",
+    "url": "/practice_nasa-apod/static/js/2.5a3ea75e.chunk.js"
+  },
+  {
+    "revision": "4ef169e897079a10287750a24d31d242",
+    "url": "/practice_nasa-apod/static/js/2.5a3ea75e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a380731d42d9aba5374",
+    "url": "/practice_nasa-apod/static/js/main.89ca3ffd.chunk.js"
+  },
+  {
+    "revision": "e27ba2c14b80eaa6fffb",
+    "url": "/practice_nasa-apod/static/js/runtime-main.105571e6.js"
+  }
+]);
